5d77dd8125a26466b6d64a985528ad19fecb6a9f
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-11-08 23:54:51 +0900

    add empty main fragment

---
+++ app/src/main/java/tokyo/tommykw/tv/fragment/MainFragment.kt
@@ -0,0 +1,7 @@
+class MainFragment : Fragment() {
+    private val videoCursorAdapter = mapOf(Int, CursorObjectAdapter)
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+}
